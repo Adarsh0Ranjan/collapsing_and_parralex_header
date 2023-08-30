f827512272ae94c216066f93b66cbb0f1bddc84d
Adarsh Ranjan Roro <adarsh.ranjan@roro.io>
2023-08-30 10:58:54 +0530

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by Adarsh Ranjan on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
